e8fac1f4f72bc6e758d2d6c3107d175cfb5be92d
Kyaw Oo Aung <aung.kyawoo@ams.com>
2024-06-19 13:59:15 +0800

    Removed excel_loader and added excel_loader.py

---
+++ main/excel_loader.py
@@ -0,0 +1,6 @@
+# this is to load excel file using window explorer
+# Loaded file will be returned to the caller module
+
+import panda as pd
+from tkinter import Tk
+from tkinter.filedialog import askopenfilename
